1cebe9ca03f20c3b47b6db003ae2fa27950a0935
ulansawalia <102313929+ulansawalia@users.noreply.github.com>
2022-05-10 16:10:48 +0700

    Add files via upload

---
+++ no2.py
@@ -0,0 +1,21 @@
+#include <stdio.h)
+
+kalimat =input()
+panjang =len(kalimat)
+for i in  range(panjang):
+    if kalimat[i]=='a':
+        print('aga', end="")
+    elif kalimat[i]=='i':
+        print('igi', end="")
+    elif kalimat[i]=='u':
+        print('ugu', end="")
+    elif kalimat[i]=='e':
+        print('ege', end="")
+    elif kalimat[i]=='o':
+        print('ogo', end="")
+    else :
+        print(kalimat[i], end="")
+    
+
+
+
